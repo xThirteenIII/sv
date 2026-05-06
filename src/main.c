79ba7430431d85024fd51d98b1fc13e810a3cc75
xThirteenIII <giuseppe.richetti@gmail.com>
2026-05-06 10:02:36 +0200

    fix: use refactored functions

--- src/main.c
+++ src/main.c
@@ -1,15 +1,12 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <stdio.h>
-//#include "process.h"
 #include "service.h"
 
 int main()
 {
-    service_t *service = service_init();
-    printf("Service:%s\t%s\t%s\t%s\n", service->cmdline, service->restart, service->fout, service->ferr);
-    //run_service(service->cmdline);
-    service_shutdown(service);
+    service_t service;
+    service_load(&service, "services/sleep.conf");
+    service_run(service.cmdline);
     return 0;
 }
